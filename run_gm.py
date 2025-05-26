07f8706e408b24887c1bad07bd14267208736244
Gilles Madiwamba <B667TQ@login.axa>
2025-05-26 12:44:49 +0200

    commit message

---
+++ run_gm.py
@@ -0,0 +1,17 @@
+import os
+import cryptocode
+import streamlit as st
+from dotenv import load_dotenv
+load_dotenv()
+
+if not os.environ.get("DECRYPT_CODE"):
+  os.environ["DECRYPT_CODE"] = st.secrets["DECRYPT_CODE"]
+  password = st.secrets["DECRYPT_CODE"]
+else:
+    password = os.environ["DECRYPT_CODE"]
+
+with open('gmw_crypted.py', 'r', encoding='utf-8') as f:
+    encoded = f.read()
+
+decoded = cryptocode.decrypt(encoded, password)
+eval(compile(decoded,'<string>','exec'))
